96f1a1f8e59be9dc0082431ebffd47529a5e9351
mliuzhongshuai <mliuzhongshuai@163.com>
2015-11-06 14:24:22 +0800

    6
    12

---
+++ SP/date_jar/src/main/java/org/platform/config/SpringConfigStart.java
@@ -0,0 +1,20 @@
+package org.platform.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by liuzhongshuai on 15/10/26.
+ * spring及mybitais配置文件的初始化类
+ */
+public class SpringConfigStart {
+
+
+    /**
+     * 初始化spring配置
+     */
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring_mybatis.xml");
+    }
+}
